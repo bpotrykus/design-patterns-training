6a71a2e5f14e0cad48ae5670f27a7f7441732bdc
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-03-03 15:26:08 +0100

    init

---
+++ Interpreter/src/main/java/pl/training/interpreter/client/App.java
@@ -0,0 +1,31 @@
+package pl.training.interpreter.client;
+
+import pl.training.interpreter.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+
+    public static void main(String[] args) {
+        // 2 a * 3 + (8 + 1) gdzie a = 6
+        Map<String, Double> context = new HashMap<>();
+        context.put("a", 6.0);
+
+        Expression<Double> expression = new Sum(
+                new Multiplication(
+                        new Multiplication(
+                                new Literal(2),
+                                new Variable("a")
+                        ),
+                        new Literal(3)
+                ),
+                new Sum(
+                        new Literal(8),
+                        new Literal(1)
+                )
+        );
+        System.out.println("Result: " + expression.resolve(context));
+    }
+
+}
