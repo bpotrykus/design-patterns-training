712fb13654a77c768a22a9f28aae7538b7b7f9c8
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-03-04 15:01:16 +0100

    init

--- tdd/src/test/java/pl/training/tdd/TemplateTest.java
+++ tdd/src/test/java/pl/training/tdd/TemplateTest.java
@@ -1,13 +1,16 @@
 package pl.training.tdd;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 public class TemplateTest {
 
@@ -16,10 +19,16 @@ public class TemplateTest {
     private Template template  = new Template("My name is ${firstName} ${lastName}");
     private Map<String, String> parameters = new HashMap<>();
 
+    @Mock
+    private List<Integer> numbers;
+
     @Before
     public void init() {
+        MockitoAnnotations.initMocks(this);
         parameters.put("firstName", "Jan");
         parameters.put("lastName", "Kowalski");
+
+        when(numbers.get(0)).thenReturn(1);
     }
 
     @Test
@@ -36,6 +45,11 @@ public class TemplateTest {
     public void shouldIgnoreAdditionalParameters() {
         parameters.put("extraParameter", "someValue");
         assertEquals(EXPECTED_RESULT, template.evaluate(parameters));
+    }
+
+    @Test
+    public void shouldReturnOne() {
+        assertTrue(numbers.get(0) == 1);
     }
 
 }
